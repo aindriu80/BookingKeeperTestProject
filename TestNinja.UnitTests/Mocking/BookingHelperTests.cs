d08a13b10b41c8a315bac3571fcf2d88c803a1a2
aindriu80 <aindriu80@gmail.com>
2018-01-13 15:39:29 +0000

    Writing inital BookingHelperTest

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -1,7 +1,38 @@
-﻿namespace TestNinja.UnitTests.Mocking
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
 {
+    [TestFixture]
     public class BookingHelperTests
     {
-        
+        [Test]
+        public void OverlappingBookingExists_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+            var repository = new Mock<IBookingRepository>();
+            repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 2,
+                    ArrivalDate = new DateTime(2018,1,15, 14, 0,0),
+                    DepartureDate = new DateTime(2018,1,20,10,0,0),
+                    Reference = "a"
+                }
+            }.AsQueryable());
+
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = new DateTime(2018, 1, 10, 14, 0, 0),
+                DepartureDate = new DateTime(2018, 1, 15, 10, 0, 0),
+            }, repository.Object);
+            
+            Assert.That(result, Is.Empty);
+        }
     }
 }
