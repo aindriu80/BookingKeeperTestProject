80630d4061e25f56bde189bb72fc5747d2bbf222
aindriu80 <aindriu80@gmail.com>
2018-01-14 11:24:28 +0000

    Writing second test & typo correction

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -32,7 +32,7 @@ namespace TestNinja.UnitTests.Mocking
         }
 
         [Test]
-        public void OverlappingBookingExists_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
             var result = BookingHelper.OverlappingBookingsExist(new Booking
             {
@@ -43,13 +43,26 @@ namespace TestNinja.UnitTests.Mocking
             
             Assert.That(result, Is.Empty);
         }
+        
+        [Test]
+        public void BookingBeforeAndFinishesBeforeAnExistingBooking_ReturnExistingBookingRefrence()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),  
+                DepartureDate = After(_existingBooking.ArrivalDate)
+            }, _repository.Object);
+            
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
 
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
         }
         
-        private DateTime AFter(DateTime dateTime)
+        private DateTime After(DateTime dateTime)
         {
             return dateTime.AddDays(1);
         }
