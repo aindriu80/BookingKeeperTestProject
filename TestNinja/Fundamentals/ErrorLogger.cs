963a5254143180178ce247c5e5cf2fc983e88be9
aindriu80 <aindriu80@gmail.com>
2018-01-23 20:54:01 +0000

    Test Class files

---
+++ TestNinja/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace TestNinja.Fundamentals
+{
+    public class ErrorLogger
+    {
+        private Guid _errorId;
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+        
+        
+        public void Log(string error)
+        {
+            
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+                
+            LastError = error; 
+            
+            // Write the log to a storage
+            // ...
+
+//            ErrorLogged?.Invoke(this, Guid.NewGuid());
+//            OnErrorLogged(Guid.NewGuid());
+
+            _errorId = Guid.NewGuid();
+            OnErrorLogged(Guid.NewGuid());
+        }
+
+//        public virtual void OnErrorLogged()
+//        {
+//        
+//        }
+
+        protected virtual void OnErrorLogged(Guid errorId)
+        {
+            ErrorLogged?.Invoke(this, errorId);   
+        }
+    }
+    
+}
